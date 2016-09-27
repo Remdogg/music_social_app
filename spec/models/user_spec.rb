17c4a0e5556f6ce71560dd32179df1d84b2c161a
learn <learn@learn03.local>
2016-09-27 13:38:51 -0700

    tested for sign up, in, and log out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  #first test for sign up page
+  it 'is a website' do
+    expect{User.new}.to_not raise_error
+  end
 end
