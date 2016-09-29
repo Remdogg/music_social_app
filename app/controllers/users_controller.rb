adf4e7f726d726db541c4a9fd8c5fe43ec73adb5
learn <learn@learn03.local>
2016-09-29 16:25:59 -0700

    followers show up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+
+  before_action :current_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = User.all
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = User.all
+    render 'show_follow'
+  end
+
   def show
     @user = User.find(params[:id])
   end
