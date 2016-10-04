3bfcaf537e77f526935a9a7e12d85d5c72a8a385
learn <learn@learn09.local>
2016-10-04 10:23:36 -0700

    association testing for bandtogethers and users

--- spec/models/bandtogether_spec.rb
+++ spec/models/bandtogether_spec.rb
@@ -26,4 +26,46 @@ RSpec.describe Bandtogether, type: :model do
   end
 
 
+  it 'can belong to one organizer' do
+    user = User.new(first_name: 'Taylor', last_name: 'Swift', email: 'joe@joe.com', password: 'password', password_confirmation: 'password')
+    bandtogether1 = Bandtogether.new(title: 'twizzle', start_time: '8pm', end_time: '8:01pm', description: 'carpool from one block to the other', concert: Concert.new())
+
+    user.id = 1
+    user.save
+
+    user.bandtogethers_as_organizer << bandtogether1
+
+    expect(bandtogether1.organizer).to match user
+
+  end
+
+  it 'can have many members' do
+
+    user1 = User.new(first_name: 'Taylor', last_name: 'Swift', email: 'joe@joe.com', password: 'password', password_confirmation: 'password')
+
+    user1.id = 1
+    user1.save
+
+    user2 = User.new(first_name: 'Joe', last_name: 'Swift', email: 'jose@jose.com', password: 'password', password_confirmation: 'password')
+
+    user2.id = 2
+    user2.save
+
+    bandtogether1 = Bandtogether.new(title: 'twizzle', start_time: '8pm', end_time: '8:01pm', description: 'carpool from one block to the other', concert: Concert.new())
+
+    bandtogether1.id = 3
+    bandtogether1.save
+
+    bandtogether1.members << user1
+
+    bandtogether1.members << user2
+
+    expect(bandtogether1.members.map {|user| user.id}).to match [user1.id, user2.id]
+
+    expect(Membership.where(bandtogether_id: "3").map {|membership| membership.user_id}).to match [user1.id, user2.id]
+
+
+  end
+
+
 end
