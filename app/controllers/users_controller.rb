1b8fb7ecd57890a46b926304a5b77a41a2d08175
learn <learn@learn09.local>
2016-10-06 16:31:37 -0700

    added admin to users and bandtogether

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,4 +75,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    if current_user.has_role? :admin
+      @user = User.find(params[:id])
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+  end
+
+
 end
