841cbcf9df345dafda8175590da920517f2dd5fa
Amy Schools <amyschools@gmail.com>
2016-10-09 15:14:21 -0700

    fixed conflicts

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,9 +1,5 @@
 class Concert < ActiveRecord::Base
-<<<<<<< HEAD
-  has_many :bandtogethers
-=======
   has_many :bandtogethers, dependent: :destroy
->>>>>>> 8da1da95ab4edef4a5cb2282fc2674fe0f82e1fb
   resourcify
   validates_presence_of :city
 
@@ -13,11 +9,7 @@ class Concert < ActiveRecord::Base
     @state = state
     @full_address = @address + " " + @city + " " + @state
   end
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> 8da1da95ab4edef4a5cb2282fc2674fe0f82e1fb
   geocoded_by :full_address
   after_validation :geocode
 end
